c394932ea960fb451051747d5aec39ef6fd34095
Wesley Chow <wgesh8@gmail.com>
2021-01-13 22:15:06 -0800

    Start navbar styling/function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-// toTopButton = $ something something i havent memorized jquery yet
+$(document).ready(function () {
 
-function toTopFunction() {
+    $("button").click(function(){
+        $("test").animate({left: '250px'});
+    });
 
-}
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+    // var prevScrollpos = window.pageYOffset;
+    // window.onscroll = function() {
+    //     var currentScrollPos = window.pageYOffset;
+    //     if (prevScrollpos > currentScrollPos) {
+    //         $("navbar").style.top = "0";
+    //     } else {
+    //         document.getElementById("navbar").style.top = "-50px";
+    //     }
+    //     prevScrollpos = currentScrollPos;
+    // }
+
+
+});
