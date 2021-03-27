354a0ddd917b5b91ab1dd0629629c92e01dc41d5
Wesley Chow <wgesh8@gmail.com>
2021-03-27 03:51:31 +0000

    Add draft data disclaimer message

--- js/media.js
+++ js/media.js
@@ -1,6 +1,5 @@
 /*
-    Use this js script for custom js within the "media" pages (virtual tour, gallery) that conflict with other
-    general page functions
+    Use this js script for custom js within the "media" pages (virtual tour, gallery) that are unused or conflict with other general page functions
 */
 
 $(document).ready(function () {
