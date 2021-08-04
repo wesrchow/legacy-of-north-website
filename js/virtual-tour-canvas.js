44dd846b8b2f018c9380aa9e2da3e2626308b70b
Wesley Chow <wgesh8@gmail.com>
2021-08-04 11:36:37 -0700

    Setup canvas interactive map

---
+++ js/virtual-tour-canvas.js
@@ -0,0 +1,15 @@
+/*
+    Use this js script for custom js within the virtual tour page that are unused or conflict with other general page functions
+*/
+
+$(document).ready(function () {
+
+    /* Variables Used Throughout */
+
+
+    /*
+    *
+    * Canvas stuff
+    *
+    * */
+}
